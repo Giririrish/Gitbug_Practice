58858f0ce9d17461f16c6d3ada815dfffad6805f
16Kharal_G <kharalgirish@gmail.com>
2022-03-08 09:51:48 +0000

    removed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// 2 + 2 is 4 minus 1 thats 3 quick mafs everyday mans on the block
-        System.out.println("Big shaq");
+
 
     }
 }
